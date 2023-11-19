416d9109a786c2d034df8bc59a34d6e05fe7355e
hodaya-kali <kalihodaya@gmail.com>
2023-11-19 22:34:45 +0200

    Add distance_from_previous_point. change the global_time to increase by 1 second but not done yet.

--- creating_synthetic_track_database.py
+++ creating_synthetic_track_database.py
@@ -73,25 +73,25 @@ def calculate_accident_point(last_x, last_y, angle):
             distance += 2
     return x, y
 
+def calculate_distance_from_previous_point(x1,x2,y1,y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def generate_route_points(accidents_id,num_points, distance_increment, noisy_points, has_accident, vehicle_id):
     route_points = []
-    current_time = int(time.time())
-
+    global_time = int(time.time())
     for i in range(num_points):
         x = y = i * distance_increment
-        global_time = current_time + i * 10  # Increase timestamp by 10 seconds for each point
+        global_time =  global_time + 1 # Increase timestamp by 1 second for each point
         accident_value = 1 if i in noisy_points else 0  # Set accident column value based on noisy points
-        route_points.append((x, y, global_time, accident_value, vehicle_id, 0, 0))  # Include vehicle_id column
-    # print("========================",route_points)
+        route_points.append((x, y, global_time, accident_value, vehicle_id, 0, 0,0))  # Include vehicle_id column
     accident_point = random.choice(noisy_points) if has_accident else -1
     for index in noisy_points:
-        last_x, last_y, _, _, _, _, _ = route_points[index - 1]
+        last_x, last_y, _, _, _, _, _,_ = route_points[index - 1]
         angle = random.uniform(0, math.pi / 4)  # Limit to 45-degree angle
 
         # Calculate the random noisy point with a 10-meter distance
         noisy_x, noisy_y = calculate_next_point(last_x, last_y, angle, distance_increment)
-        route_points.insert(index, (noisy_x, noisy_y, current_time + index * 10, 0, vehicle_id,0,0))
+        route_points.insert(index, (noisy_x, noisy_y,global_time, 0, vehicle_id,0,0,0))
 
         # Remove the next point after the noisy point
         route_points.pop(index + 1)
@@ -100,7 +100,7 @@ def generate_route_points(accidents_id,num_points, distance_increment, noisy_poi
         if has_accident and index == accident_point:
             accidents_id.append(vehicle_id)
             accident_x, accident_y = calculate_accident_point(last_x, last_y, angle)
-            route_points[accident_point] = (accident_x, accident_y, current_time + accident_point * 10, 1, vehicle_id,0,0)
+            route_points[accident_point] = (accident_x, accident_y, global_time, 1, vehicle_id,0,0,0)
             # print(vehicle_id) #TODO -  delete this line.
 
     return route_points,accidents_id
@@ -109,7 +109,7 @@ def generate_route_points(accidents_id,num_points, distance_increment, noisy_poi
 def write_to_csv(filename, data):
     with open(filename, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['local_x', 'local_y', 'global_time', 'accident', 'vehicle_id','velocity', 'acceleration'])
+        csv_writer.writerow(['local_x', 'local_y', 'global_time', 'accident', 'vehicle_id','velocity', 'acceleration','distance_from_previous_point'])
         for row in data:
             csv_writer.writerow(row)
 
@@ -129,17 +129,25 @@ def creating_synthetic_track_database(tracks_amount):
         has_accident = random.random() < 0.2  # 50% probability of having an accident
         route_points,accidents_id = generate_route_points(accidents_id,num_points, distance_increment, noisy_points, has_accident,
                                              f'vehicle_{vehicle_id}')
+
+        # calculate the velocity ,acceleration and the distance from the previous point for all the points.
         for i in range(1, len(route_points)):
-            x1, y1, t1, _, _, _, _ = route_points[i - 1]
-            x2, y2, t2, _, _, _, _ = route_points[i]
+            x1, y1, t1, _, _, _, _, _ = route_points[i - 1]
+            x2, y2, t2, _, _, _, _ , _= route_points[i]
             velocity = calculate_velocity(x1, y1, t1, x2, y2, t2)
-            route_points[i] = route_points[i][:5] + (velocity, 0)
+            route_points[i] = route_points[i][:5] + (velocity, 0 , 0)
 
         for i in range(1, len(route_points)):
-            _, _, t1, _, _, v1, _ = route_points[i - 1]
-            _, _, t2, _, _, v2, _ = route_points[i]
+            _, _, t1, _, _, v1, _,_ = route_points[i - 1]
+            _, _, t2, _, _, v2, _,_ = route_points[i]
             acceleration = calculate_acceleration(v1, v2, t1, t2)
-            route_points[i] = route_points[i][:6] + (acceleration,)
+            route_points[i] = route_points[i][:6] + (acceleration, 0, )
+
+        for i in range(1, len(route_points)):
+            x1, y1, _, _, _, _, _,_ = route_points[i - 1]
+            x2, y2, _, _, _, _, _ ,_= route_points[i]
+            distance_from_previous_point = calculate_distance_from_previous_point(x1, x2, y1, y2)
+            route_points[i] = route_points[i][:7] + (distance_from_previous_point,)
         combined_route_points.extend(route_points)
 
         vehicle_data[f'vehicle_{vehicle_id}'] = has_accident
@@ -149,7 +157,7 @@ def creating_synthetic_track_database(tracks_amount):
             for i in range(len(combined_route_points)):
                 if combined_route_points[i][4] == vehicle_id:
                     combined_route_points[i] = (combined_route_points[i][0], combined_route_points[i][1],
-                                               combined_route_points[i][2], 1, vehicle_id, combined_route_points[i][5], combined_route_points[i][6])
+                                               combined_route_points[i][2], 1, vehicle_id, combined_route_points[i][5], combined_route_points[i][6],combined_route_points[i][7])
     if tracks_amount == 1500:
         write_to_csv('small_vehicle_tracks_database.csv', combined_route_points)
     else:
@@ -160,7 +168,7 @@ if __name__ == '__main__':
     small_database = 1500 # A parameter of the amount of tracks for the small database
     big_database = 29127 # A parameter of the amount of tracks for the big database
     accidents_id = creating_synthetic_track_database(small_database)
-    creating_synthetic_track_database(big_database)
+    # creating_synthetic_track_database(big_database)
     data_frame = main.import_data()
     random_id = random.randint(0,len(accidents_id)-1)  # Increase distance by a random value between 2 and 8 meters in case of an accident
     drawing_track(data_frame, accidents_id[0])
